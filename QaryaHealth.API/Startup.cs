6147925a6260321c627d1742c4e0adb3b08a4672
HeikalE <eslam.heikal.ext@aptar.com>
2022-03-30 15:25:46 +0200

    using swagger in production

--- QaryaHealth.API/Startup.cs
+++ QaryaHealth.API/Startup.cs
@@ -38,9 +38,10 @@ namespace QaryaHealth.API
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "QaryaHealth.API v1"));
             }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "QaryaHealth.API v1"));
 
             app.UseHttpsRedirection();
 
